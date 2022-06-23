e8f679a17b7c664afbd4a2313a5245897cb067aa
Daniel <danielideho@gmail.com>
2022-06-23 10:57:41 +0200

    bot is aangemaakt

---
+++ bot/bot.go
@@ -0,0 +1,56 @@
+package bot
+
+import (
+	"fmt"
+
+	"github.com/akhil/discord-ping/config"
+	"github.com/bwmarrin/discordgo"
+)
+
+var BotID string
+var goBot *discordgo.Session
+
+func Start() {
+	goBot, err := discordgo.New("Bot " + config.Token)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	u, err := goBot.User("@me")
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	BotID = u.ID
+
+	goBot.AddHandler(messageHandler)
+
+	err = goBot.Open()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println("Bot is runnendeee broer!")
+}
+
+//dit is een fucn genaamd messagehandler waarom is deze functie er? nou om ervoor te zorgen dat de bot een mesage stuurt moet er een functie voor toegevoegd worden.
+//en dat is deze messageHandler je ziet dat er een if statement is waarom? nou als messagecontent (wat iedmand stuurt) gelijk is aan !ping of !danny dan reageerd hij met ChannelMessageSend.
+func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	if m.Author.ID == BotID {
+		return
+	}
+
+	if m.Content == "!ping" {
+		_, _ = s.ChannelMessageSend(m.ChannelID, "ping mij niet neger")
+	}
+
+	if m.Content == "!danny" {
+		_, _ = s.ChannelMessageSend(m.ChannelID, "danny is gay lol")
+	}
+
+}
